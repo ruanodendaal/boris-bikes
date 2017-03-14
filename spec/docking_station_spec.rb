bebd9a6817b5126bbcc2b0b8caeeb1c254bc9e4f
Ruan Odendaal <ruanodendaal@gmail.com>
2017-03-14 16:59:28 +0000

    add additonal test for working bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,10 @@ require 'docking_station'
 
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
+
+  it 'returns a bike that is working' do
+   bike = subject.release_bike
+   expect(bike).to be_working
+  end
+
 end
