445eee8cf01f996c72df42c9daec3b2a06e04402
Ruan Odendaal <ruanodendaal@gmail.com>
2017-03-15 18:52:41 +0000

    change variable to be an array plus changed name to be more than one bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,16 +1,21 @@
 require_relative 'bike'
 
 class DockingStation
-  attr_reader :bike
 
-  def release_bike
-    raise "No bikes available" unless @bike
-    @bike
+  def initialize
+    @stored_bikes = []
   end
 
-  def dock_bike(bike)
-    raise "Docking station full" if @bike
-    @bike = bike
+  attr_reader :stored_bikes
+
+  def release_bike
+    raise "No bikes available" if @stored_bikes.empty?
+    @stored_bikes.pop
+  end
+
+  def dock_bike(stored_bikes)
+    raise "Docking station full" if @stored_bikes.size >= 20
+    @stored_bikes << stored_bikes
   end
 
 end
