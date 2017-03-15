eada0c80f0113e7a3b48488b3f08002de5430340
Ruan Odendaal <ruanodendaal@gmail.com>
2017-03-15 12:46:31 +0000

    Include attribute reader to return docked bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,4 +10,17 @@ describe DockingStation do
 
   it { is_expected.to respond_to(:dock_bike).with(1).argument }
 
+  it { is_expected.to respond_to(:bike) }
+
+  it 'docks something' do
+    bike = Bike.new
+    expect(subject.dock_bike(bike)).to eq bike
+  end
+
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock_bike(bike)
+    expect(subject.bike).to eq bike
+  end
+
 end
