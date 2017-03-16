ee86e35337d848bc6f41f8beaadc440db9567b92
Ruan Odendaal <ruanodendaal@gmail.com>
2017-03-16 16:03:13 +0000

    create predicate methods and use constant

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,20 +2,32 @@ require_relative 'bike'
 
 class DockingStation
 
+  DEFAULT_CAPACITY = 20
+
+  attr_reader :stored_bikes
+
   def initialize
     @stored_bikes = []
   end
 
-  attr_reader :stored_bikes
-
   def release_bike
-    raise "No bikes available" if @stored_bikes.empty?
+    raise "No bikes available" if empty?
     @stored_bikes.pop
   end
 
   def dock_bike(stored_bikes)
-    raise "Docking station full" if @stored_bikes.size >= 20
+    raise "Docking station full" if full?
     @stored_bikes << stored_bikes
+  end
+
+  private
+
+  def full?
+    @stored_bikes.size >= DEFAULT_CAPACITY
+  end
+
+  def empty?
+   @stored_bikes.empty?
   end
 
 end
