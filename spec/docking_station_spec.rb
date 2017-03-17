86edffd8fc056cde0dd6210e61fab0b28f74dbfe
Ruan Odendaal <ruanodendaal@gmail.com>
2017-03-17 09:48:51 +0000

    change to use describe initialization

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,6 +8,23 @@ describe DockingStation do
    expect(bike).to be_working
   end
 
+  describe 'initialization' do
+    subject { DockingStation.new }
+    let(:bike) { Bike.new }
+
+    it 'has variable capacity' do
+      docking_station = DockingStation.new(50)
+      50.times { docking_station.dock(bike) }
+      expect { docking_station.dock(bike) }.to raise_error "Docking station full"
+    end
+
+    it "raises an error using default capacity" do
+      subject.capacity.times { subject.dock(bike) }
+      expect { subject.dock(bike) }.to raise_error "Docking station full"
+    end
+
+  end
+
   describe '#dock' do
     it { is_expected.to respond_to(:dock).with(1).argument }
 
@@ -16,16 +33,6 @@ describe DockingStation do
       expect(subject.dock(bike)).to eq [bike]
     end
 
-    it "raises an error when docking station is at full capacity" do
-      docking_station = DockingStation.new(50)
-      50.times { docking_station.dock(Bike.new) }
-      expect { docking_station.dock(Bike.new) }.to raise_error "Docking station full"
-    end
-
-    it "raises an error using default capacity" do
-      subject.capacity.times { subject.dock(Bike.new) }
-      expect { subject.dock(Bike.new) }.to raise_error "Docking station full"
-    end
   end
 
   describe '#capacity' do
