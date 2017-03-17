5c012b8b2a2380f4aa7f2c84cedcc5f78d5076f1
Ruan Odendaal <ruanodendaal@gmail.com>
2017-03-17 12:18:50 +0000

    mock behaviour using double

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,15 +2,17 @@ require 'docking_station'
 
 describe DockingStation do
 
+  let(:bike) {double :bike}
+
   it 'returns a bike that is working' do
-   bike = Bike.new
-   subject.dock(bike)
-   expect(bike).to be_working
+    allow(bike).to receive(:working?).and_return(true)
+    subject.dock(bike)
+    expect(bike).to be_working
   end
 
   describe 'initialization' do
     subject { DockingStation.new }
-    let(:bike) { Bike.new }
+    let(:bike) { double :bike }
 
     it 'has variable capacity' do
       docking_station = DockingStation.new(50)
@@ -29,13 +31,11 @@ describe DockingStation do
     it { is_expected.to respond_to(:dock).with(1).argument }
 
     it "docks working bikes" do
-      bike = Bike.new
       expect(subject.dock(bike)).to eq [bike]
     end
 
     it "docks broken bikes" do
-      bike = Bike.new
-      bike.report_broken
+      allow(bike).to receive(:report_broken).and_return(true)
       expect(subject.dock(bike)).to eq [bike]
     end
   end
@@ -48,22 +48,21 @@ describe DockingStation do
 
   describe '#release_bike' do
     it 'releases a bike' do
-      bike = Bike.new
-      subject.dock(bike)
+      allow(bike).to receive(:broken?)
+      subject.dock bike
       expect(subject.release_bike).to eq bike
     end
 
     it "releases working bikes" do
-      bike = Bike.new
+      allow(bike).to receive_messages(:broken? => false, :working? => true)
       subject.dock(bike)
-      subject.release_bike
-      expect(bike).to be_working
+      released_bike = subject.release_bike
+      expect(released_bike).to be_working
     end
 
     it "does not release broken bikes" do
-      bike = Bike.new
-      subject.dock(bike)
-      bike.report_broken
+      allow(bike).to receive_messages(:broken? => true, :report_broken => true)
+      subject.dock bike
       expect{ subject.release_bike}.to raise_error "This bike is broken"
     end
 
