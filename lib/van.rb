1f8d831dc9c347b3423b9aa26b2f083f40234f8e
Ruan Odendaal <ruanodendaal@gmail.com>
2017-03-17 17:59:46 +0000

    select broken bikes only

--- lib/van.rb
+++ lib/van.rb
@@ -7,6 +7,9 @@ attr_reader :broken_bikes
 
   def take_broken_bikes(stored_bikes)
     broken_bikes = stored_bikes.select {|x| x.broken? }
+    # still need to inform docking_station to remove the broken bikes
+    stored_bikes = stored_bikes - broken_bikes
+    return broken_bikes
   end
 
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -6,18 +6,22 @@ describe Van do
     bike = Bike.new
     bike.report_broken
     station = (DockingStation.new).dock(bike)
-    expect(subject.take_broken_bikes(station)).to eq station
+    expect(subject.take_broken_bikes(station)).to eq [bike]
   end
 
-  # it 'should collect all broken bikes' do
-  #   bike = Bike.new
-  #   bike.report_broken
-  #   bike2 = Bike.new
-  #   bike.report_broken
-  #   bike3 = Bike.new
-  #   station = (DockingStation.new).dock(bike)
-  #   expect(subject.take_broken_bikes(station)).to eq station
-  # end
+  it 'should collect all broken bikes' do
+    bike1 = Bike.new
+    bike1.report_broken
+    bike2 = Bike.new
+    bike2.report_broken
+    bike3 = Bike.new
+    station = (DockingStation.new)
+    station.dock(bike1)
+    station.dock(bike2)
+    array = station.dock(bike3)
+
+    expect(subject.take_broken_bikes(array)).to eq [bike1, bike2]
+  end
 
 
 end
