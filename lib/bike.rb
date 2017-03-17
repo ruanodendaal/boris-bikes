c411502b90364eaa69b21616071924222bcb1c93
Ruan Odendaal <ruanodendaal@gmail.com>
2017-03-17 11:29:47 +0000

    add ability to report broken bikes

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,6 +1,19 @@
 class Bike
+attr_reader :broken
+
+  def initialize
+    @broken = false
+  end
 
   def working?
-    true
+    @broken == false ? true : false
+  end
+
+  def report_broken
+    @broken = true
+  end
+
+  def broken?
+    @broken
   end
 end
